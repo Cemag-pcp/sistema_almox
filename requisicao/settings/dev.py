c6dfffd2ffc4934d072170893894b4f3e9f51b5a
luan <luan640@gmail.com>
2024-10-18 08:18:41 -0300

    ligacao com banco de dados

---
+++ requisicao/settings/dev.py
@@ -0,0 +1,30 @@
+from requisicao.settings.base import *
+
+# Configurações específicas de desenvolvimento
+DEBUG = env.bool('DEBUG', default=True)
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+
+# Banco de dados para desenvolvimento
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
+        'OPTIONS': {
+            'options': '-c search_path=almoxarifado_v2',
+        },
+    }
+}
+
+# Configurações adicionais para desenvolvimento (opcional)
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
