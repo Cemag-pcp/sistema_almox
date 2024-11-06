7b661ff577701b8354a5ce520edb95482e7a39d3
luan <luan640@gmail.com>
2024-11-06 14:30:47 -0300

    req

--- solicitacao/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import *
-
-admin.site.register(SolicitacaoTransferencia)
-admin.site.register(SolicitacaoCadastroItem)
-admin.site.register(SolicitacaoRequisicao)
-admin.site.register(SolicitacaoNovaMatricula)
